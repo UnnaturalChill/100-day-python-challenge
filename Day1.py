4516dbb7ff53dd81b0669839ed132a016c7e5795
Vetsuu <eskelinen.veeti@gmail.com>
2023-02-19 18:31:23 +0200

    Added files for the first two days

---
+++ Day1.py
@@ -0,0 +1,9 @@
+print("Veeti")
+print("18.2.2023")
+
+print("""I am signing up for Replit's 100 days of python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone whenever I happen to be. 
+No excuses for not coding from the middle of the field!""")
+
+print("I am feeling confident!")
---
+++ Day2.py
@@ -0,0 +1,19 @@
+print("Getting to know you!")
+
+print()
+
+myName = input("What's your name?: ")
+favouriteFood = input("What's your favourite food?: ")
+favouriteMusic = input("What's your favourite music?: ")
+whereILive = input("Where do you live?: ")
+
+print()
+
+print("You are")
+print(myName)
+print("You're probably hungry for")
+print(favouriteFood)
+print("and you're definitely getting your groove on to")
+print(favouriteMusic)
+print("living in the amazing")
+print(whereILive)
