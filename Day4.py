515ae0a837998d303bf7057aaa7574231997d04c
Vetsuu <Ep1taph@pm.me>
2023-02-21 16:04:05 +0200

    Updated variable name

--- Day4.py
+++ Day4.py
@@ -5,8 +5,8 @@ print()
 placeName = input("Give your world a name: ")
 characterName = input("Give your character a name: ")
 characterLocation = input("Give your characters location in the world: ")
-anotherCharacterLocation = input("Give another location: ")
+location = input("Give another location: ")
 
 
 print(" A long long time ago in a faraway land Called", "\033[32m", placeName, "\033[0m", "there was a fairy called", "\033[36m", characterName, ".", 
-    "\033[0m", "They liked to freeze frogs at the", "\033[34m", characterLocation, "\033[0m", "near the", "\033[31m", anotherCharacterLocation, ".")
+    "\033[0m", "They liked to freeze frogs at the", "\033[34m", characterLocation, "\033[0m", "near the", "\033[31m", location, ".")
