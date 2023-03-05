4ed8e3d13bd62524c667fc16f7621210f0638c64
UnnaturalChill <Ep1taph@pm.me>
2023-03-05 18:57:54 +0200

    Added code for 18th day of coding

---
+++ Days11-20/Day18.py
@@ -0,0 +1,30 @@
+import random
+
+print("Guess the number game")
+
+retries = 0
+rndNumber = random.randint(1, 1000)
+guess = 0
+
+while True:
+  if guess < 0:
+    print()
+    print("You have to guess a positive number but fine.")
+    exit()
+  
+  print()
+  guess = int(input("Guess a number between 1 and 1000 > "))
+  print()
+  if guess == rndNumber:
+    print("Congratulations! You guessed the number!")
+    break
+  elif guess < rndNumber:
+    print("Your guess was too low.")
+    retries += 1
+    continue
+  elif guess > rndNumber:
+    print("Your guess was too high.")
+    retries += 1
+    continue
+
+input("It took you {} retries to get it correct.".format(retries))
