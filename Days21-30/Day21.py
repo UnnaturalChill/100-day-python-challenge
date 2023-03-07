edbd98959c0183cc72f564894577f80e82018255
UnnaturalChill <Ep1taph@pm.me>
2023-03-07 11:53:21 +0200

    Added code for 21st day of coding

--- Days21-30/Day21.py
+++ Days21-30/Day21.py
@@ -1,9 +1,17 @@
 print("Math Game")
 print()
 
+scoreCounter = 0
 multiples = int(input("Name your multiples: "))
+print()
 
 for i in range(1, 11):
-  Q1 = int(input(f"{i} x {multiples} = "))
-  if Q1 % multiples == 0:
-    print(f"{i} x {multiples} = {Q1}")
+  question = int(input(f"{i} x {multiples} = "))
+  if question == i * multiples:
+    print("You got it right. Great work!")
+    print()
+    scoreCounter += 1
+  else: 
+    print(f"You got it wrong. Right answer is {i * multiples}")
+    print()
+print(f"You scored {scoreCounter} out of 10")
