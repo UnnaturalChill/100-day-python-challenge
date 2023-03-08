73e4a215e6d13d6e7f2c2c629e97cf7614231a8b
UnnaturalChill <Ep1taph@pm.me>
2023-03-08 18:13:38 +0200

    Added code for 26th day of coding

---
+++ Days21-30/Day26.py
@@ -0,0 +1,37 @@
+from replit import audio
+import os, time
+
+# Code only works on Replit
+def play():
+  source = audio.play_file('audio.wav')
+  source.paused = False
+  while True:
+    stopAudio = int(input("Press 2 to go back > "))
+    if stopAudio == 2:
+      source.paused = True
+      return
+    else:
+      continue
+
+
+while True:
+  os.system("clear")
+  print("MyPOD Music Player")
+  print()
+  
+  time.sleep(1)
+  print("Press 1 to Play")
+  print("Press 2 to Exit")
+  time.sleep(1)
+  
+  print()
+  print("Press anything else to see the manu again")
+  print()
+  
+  userInput = input("")
+  if userInput == "1":
+    play()
+  elif userInput == "2":
+    break
+  else:
+    continue
